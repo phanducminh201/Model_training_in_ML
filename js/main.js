629cd9ea37d1036be0f4a2aa8547b81e312dbe94
Minh <minhphann215@gmail.com>
2025-04-23 15:00:50 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// console.log(document.getElementById('element'))
+// console.log(document.getElementsByTagName('div'))
+// console.log(document.getElementsByClassName('box'))
+
+
+
+function changeColor(){
+    document.getElementById('box').classList.add('active')
+}
