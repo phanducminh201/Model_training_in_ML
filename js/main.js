70c0a481ca258bc917a32c5304ce51bde98697f1
Đức Minh <minhphann215@gmail.com>
2025-04-23 15:12:45 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// console.log(document.getElementById('element'))
-// console.log(document.getElementsByTagName('div'))
-// console.log(document.getElementsByClassName('box'))
-
-
-
-function changeColor(){
-    document.getElementById('box').classList.add('active')
-}
